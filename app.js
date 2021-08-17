0c207ce1d5cb766cb2a21bbaec7fb796824c11fa
mhamzakhan048 <kbrother129@gmail.com>
2021-08-17 15:16:43 +0500

    checking form

---
+++ app.js
@@ -0,0 +1,19 @@
+function showpass() {
+    let x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  let register = () => {
+    let username = document.getElementById("username");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+
+
+
+  
+}
+
